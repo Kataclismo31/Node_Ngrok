4cbde71177326e3a1b576dd649e64eb362128464
Kataclismo31 <140910781+Kataclismo31@users.noreply.github.com>
2023-11-12 09:17:55 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+//Creando servidor web en expresss
+const express = require('express');
+const app = express();
+
+//inicializar puerto Serial
+var SerialPort = require("serialport");
+const { arduinoCOMPort } = require('./arduinoCOMPort');
+
+//Listar puerto
+var port = 3000;
+
+//Definir Velocidad
+var arduinoSerialPort = new SerialPort(arduinoCOMPort, {baudRate: 9600});
+
+//apertura de Puertos
+arduinoSerialPort.on('open',function() {
+  console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+});
+
+//Ruta de Test
+app.get('/', function (req, res) {
+    return res.send('App para encender led');
+})
+
+//Ruta de acciones
+app.get('/:action', function (req, res) {
+   var action = req.params.action || req.param('action');
+    if(action == 'o'){
+        arduinoSerialPort.write("1");
+        return res.send('Led Encedido');
+    } 
+    if(action == 'off') {
+        arduinoSerialPort.write("0");
+        return res.send("Apagar Led");
+    }
+    return res.send('Action: ' + action);
+});
+
+app.listen(port, function () {
+  console.log('listening on port http://0.0.0.0:' + port + '!');
+});
---
+++ arduinoCOMPort.js
@@ -0,0 +1,3 @@
+//Listar puerto serial (Windows, otros sistemas operativos)
+var arduinoCOMPort = "COM3";
+exports.arduinoCOMPort = arduinoCOMPort;
